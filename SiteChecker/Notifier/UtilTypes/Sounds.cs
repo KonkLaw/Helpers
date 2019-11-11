d9b78a0cbbe5bf606dc2e07f62b6b0295fbfae5e
KonkLaw <polishchuk.artyom@gmail.com>
2019-11-11 21:26:01 +0300

    added: embeded sound

---
+++ SiteChecker/Notifier/UtilTypes/Sounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Media;
+
+namespace Notifier.UtilTypes
+{
+	class Sounds
+	{
+		private readonly SoundPlayer soundsPlayer;
+		public static Sounds Instance = new Sounds();
+
+		private Sounds()
+		{
+			soundsPlayer = new SoundPlayer(Properties.Resources.Windows_Foreground);
+		}
+
+		public void Play()
+		{
+			soundsPlayer.Play();
+		}
+	}
+}
